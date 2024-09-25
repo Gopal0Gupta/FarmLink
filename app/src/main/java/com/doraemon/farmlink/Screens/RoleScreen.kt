17eafc649c8f1ba01dddd141cf5b64c20a922415
Gopal Gupta <k7523337@gmail.com>
2024-09-26 02:34:14 +0530

    FarmLink

--- app/src/main/java/com/doraemon/farmlink/Screens/RoleScreen.kt
+++ app/src/main/java/com/doraemon/farmlink/Screens/RoleScreen.kt
@@ -1,6 +1,5 @@
 package com.doraemon.farmlink.Screens
 
-import android.media.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.Image
@@ -11,14 +10,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -26,12 +21,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.doraemon.farmlink.AuthState
 import com.doraemon.farmlink.R
 import com.doraemon.farmlink.authViewModel
 
@@ -65,7 +58,7 @@ fun RoleScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                painter = painterResource(R.drawable.img),
+                painter = painterResource(R.drawable.farmericon),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
@@ -73,9 +66,13 @@ fun RoleScreen(
                     .clip(CircleShape)
                     .border(2.dp, Color.Black, CircleShape)
             )
-            Text( text = "Farmer")
+            Text(
+                text = "Farmer",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
-        Spacer(modifier.height(10.dp))
+        Spacer(modifier.height(15.dp))
         Column(
             modifier = Modifier.clickable {
                 authViewModel.saveUserRoleToFirestore(context,"buyer")
@@ -85,7 +82,7 @@ fun RoleScreen(
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Image(
-                painter = painterResource(R.drawable.img_1),
+                painter = painterResource(R.drawable.buyericon),
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
@@ -93,7 +90,11 @@ fun RoleScreen(
                     .clip(CircleShape)
                     .border(2.dp, Color.Black, CircleShape)
             )
-            Text( text = "Buyer")
+            Text(
+                text = "Buyer",
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
