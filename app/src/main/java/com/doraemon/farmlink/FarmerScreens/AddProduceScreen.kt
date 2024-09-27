daea5544e7cb9fff6fd95cb4d216b09b6846db6d
Gopal Gupta <k7523337@gmail.com>
2024-09-27 18:12:40 +0530

    FarmLink

--- app/src/main/java/com/doraemon/farmlink/FarmerScreens/AddProduceScreen.kt
+++ app/src/main/java/com/doraemon/farmlink/FarmerScreens/AddProduceScreen.kt
@@ -1,18 +1,110 @@
 package com.doraemon.farmlink.FarmerScreens
 
+import android.content.Context
+import android.net.Uri
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import com.doraemon.farmlink.R
+import com.google.firebase.storage.FirebaseStorage
 
 @Composable
 fun AddProduceScreen() {
-    LazyColumn {
-        items(5) { index ->
-            Text(text = "Add Produce Field $index", modifier = Modifier.padding(16.dp))
+
+    var imageUri by remember{
+        mutableStateOf<Uri?>(null)
+    }
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.GetContent()){ uri: Uri? ->
+        imageUri = uri
+    }
+    val context = LocalContext.current
+
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val painter: Painter = if (imageUri != null){
+                rememberAsyncImagePainter(imageUri)
+            }else{
+                painterResource(id = R.drawable.gallery)
+            }
+            Image(
+                painter = painter,
+                contentDescription = null,
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .height(250.dp)
+                    .width(250.dp)
+                    .border(
+                        width = 2.dp, color = Color.Black, shape = RoundedCornerShape(8.dp)
+                    )
+                    .clickable {
+                        launcher.launch("image/*")
+                    }
+                )
+            Spacer(modifier = Modifier.height(20.dp))
+            Button(onClick = {
+                if (imageUri != null){
+                    imageUri.let { uri ->  
+                        uploadImageToFirebase(uri,context)
+                    }
+                }else{
+                    Toast.makeText(context,"Please Upload Image From Gallery",Toast.LENGTH_SHORT).show()
+                }
+            }, shape = RoundedCornerShape(5.dp)) {
+                Text(text = "Upload Image")
+            }
         }
+    }
+}
+
+fun uploadImageToFirebase(uri: Uri?, context: Context) {
+    val storage = FirebaseStorage.getInstance()
+    val storageRefrence = storage.reference
+    val imageRefrence = storageRefrence.child("image/"+uri!!.lastPathSegment)
+
+    val uploadTask = uri.let { imageRefrence.putFile(it) }
+
+    uploadTask.addOnSuccessListener {
+        Toast.makeText(context,"Image Upload Complete",Toast.LENGTH_SHORT).show()
+    }.addOnFailureListener{
+        Toast.makeText(context,"Image Upload Complete",Toast.LENGTH_SHORT).show()
     }
 }
 
